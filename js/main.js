985893a75080d460f3c567060ab615654f998557
R1PRTR <gegor675@gmail.com>
2024-03-23 15:32:08 +0300

    XD

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	
+	$(".menu a ").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 2200);
+	});
+
+
+
+
+
+
+
+
+
+});
+ 
